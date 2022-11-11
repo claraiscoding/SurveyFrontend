3ac696fc899b7c598308d127d5ebbcdf179e94e6
claraiscoding <clara.brusamolino@gmail.com>
2022-11-11 15:51:23 +0100

    solo le surveys non fatte si aprono yay

--- src/app/survey-list/survey-list.component.ts
+++ src/app/survey-list/survey-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { RestApiService } from "../services/rest-api.service";
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {LoginComponent} from "../login/login.component";
 import {PartecipateComponent} from "../partecipate/partecipate.component";
 
 @Component({
@@ -15,7 +14,8 @@ export class SurveyListComponent implements OnInit {
   public mail: string = "";
   public error: string  = "";
   public surveyTable: SURVEY_TABLE[] = [];
-  public displayedColumns: string[] = ['Name', 'Category', 'Publishing Date', 'Ending Date', 'Created By'];
+  public surveyDone: SURVEY_TABLE[] = [];
+  public displayedColumns: string[] = ['id', 'Name', 'Category', 'Publishing Date', 'Ending Date', 'Created By'];
   public dataSource = this.surveyTable;
 
   constructor(private route: ActivatedRoute, private router: Router, private ras: RestApiService, public dialog: MatDialog) { }
@@ -37,6 +37,12 @@ export class SurveyListComponent implements OnInit {
       }).catch((err) => {
         this.error = "Surveys not found";
       });
+    await this.ras.callApi('http://localhost:8080/survey/api/users/' + this.mail, 'GET', '')
+      .then((res) => {
+        this.surveyDone = res["surveys"];
+      }).catch((err) => {
+        this.error = "Surveys not found";
+      });
   }
 
   public async openSurvey() {
@@ -44,14 +50,21 @@ export class SurveyListComponent implements OnInit {
 
     config.disableClose = true;
     config.id           = "survey-component";
-    config.width        = "600px";
-    config.height       = "400px";
+    config.width        = "1000px";
+    config.height       = "800px";
     config.data         = {
       title: "Survey",
       component: "survey"
     }
-
     const dialogRef = this.dialog.open(PartecipateComponent, config);
+  }
+
+  public checkSurvey (row: any) {
+    let ret = true;
+    this.surveyDone.forEach(element => {
+      if (element.id === row['id']) ret = false;
+    });
+    return ret;
   }
 }
 
